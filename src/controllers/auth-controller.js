f0a8abbc81497d48fe13283a6cd39299931063de
Breno Freitas <brenofreitasmendes@gmail.com>
2025-02-26 23:17:13 -0300

    feat: new password encrypt implementation

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,21 +1,30 @@
 import jwt from "jsonwebtoken";
 import userRepository from "../repositories/user-repository.js";
+import bcrypt from 'bcrypt';
+
 
 export const login = async (req, res) => {
-  const { email, password } = req.body;
+    const { email, password } = req.body;
+    
+    try {
+        const user = await userRepository.findUserByEmail(email);
+        if (!user) {
+            return res.status(401).json({ message: "Credenciais inválidas" });
+        }
 
-  try {
-    const user = await userRepository.findUserByEmail(email);
-    if (!user || user.password !== password) {
-      return res.status(401).json({ message: "Credenciais inválidas" });
+        // Comparar a senha fornecida com a senha criptografada
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        console.log('passwordMatch: ', passwordMatch);
+        if (!passwordMatch) {
+            return res.status(401).json({ message: "Credenciais inválidas" });
+        }
+
+        const token = jwt.sign({ userId: user._id }, "seu_segredo_jwt");
+        res.json({ token });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Erro ao autenticar usuário" });
     }
-
-    const token = jwt.sign({ userId: user._id }, "seu_segredo_jwt");
-    res.json({ token });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Erro ao autenticar usuário" });
-  }
 };
 
 export const verifyToken = (req, res) => {
