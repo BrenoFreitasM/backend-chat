1c97463226c555017672868bf4cb8a646233d9e9
Breno Freitas <brenofreitasmendes@gmail.com>
2025-02-27 19:36:34 -0300

    feat: ajuste chat websocket

--- src/controllers/message-controller.js
+++ src/controllers/message-controller.js
@@ -1,15 +1,19 @@
 import { saveMessage } from "../repositories/message-repository.js";
 
-export const sendMessage = async (data, wss) => {
+export const sendMessage = async (data, wss, sender) => {
   try {
     const message = JSON.parse(data);
     console.log("📩 Mensagem recebida:", message);
+    const sender = message.sender;
+    // Adiciona o remetente à mensagem
+    const fullMessage = { ...message, sender };
 
-    await saveMessage(message);
+    await saveMessage(fullMessage);
 
     wss.clients.forEach((client) => {
+      console.log('Clientes: ', client)
       if (client.readyState === 1) {
-        client.send(JSON.stringify(message));
+        client.send(JSON.stringify(fullMessage));
       }
     });
   } catch (error) {
